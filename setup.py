d8d31ca4877661494ec179d868ae437b444959f8
AndreasGalbani <168494634+AndreasGalbani@users.noreply.github.com>
2024-12-13 04:51:18 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,8 @@ setup(
     name='phonky',
     version='0.1.0',
     py_modules=['tsm'],
-    packages=find_packages()
+    packages=find_packages(),
+    package_data={
+        'pyarmor_runtime_000000': ['pyarmor_runtime.pyd'],
+    },
 )
